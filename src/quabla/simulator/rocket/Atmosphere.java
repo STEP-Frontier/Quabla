d40477ac8aeceae50b75df3535fdeddf60a0e24f
tada_no <masamichi.ikeno@gmail.com>
2026-07-16 13:29:44 +0900

    圧力について修正。磁気偏角の西偏東偏の不応問題を修正

--- src/quabla/simulator/rocket/Atmosphere.java
+++ src/quabla/simulator/rocket/Atmosphere.java
@@ -37,7 +37,9 @@ public class Atmosphere {
 		double pressure0 = 101325;//[Pa] atomospheric pressure at 0 m
 		double gamma = -5.256;
 
-		pressure = pressure0 * Math.pow(288.15 / (273.15 + getTemperture(alt)), gamma);
+		//pressure = pressure0 * Math.pow(288.15 / (273.15 + getTemperture(alt)), gamma);
+		pressure = pressure0*Math.pow((273.15 + temprture0)/(273.15 + getTemperture(alt)), gamma);
+		
 
 		return pressure ;
 	}
